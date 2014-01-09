28deee61949f26552cc969f92ae6f176fc6479bf
Tom Howe <tom.howe@mailonline.co.uk>
2014-01-09 17:45:38 +0000

    tom: add setup-redis.sh

---
+++ setup-redis.sh
@@ -0,0 +1,13 @@
+REDIS_MASTER=$1
+
+pkgin in redis
+
+# add slave line
+if [ "$REDIS_MASTER" ]; then
+    echo "This Redis will be a slave of master: $REDIS_MASTER" 
+    echo "slaveof $REDIS_MASTER 6379" >> /opt/local/redis.conf
+else 
+    echo "This Redis will be a master"
+fi
+
+svcadm enable redis
