64857342ec15d8cdd8d363fb37192eff44978044
Tom Howe <tom.howe@mailonline.co.uk>
2014-01-10 10:01:12 +0000

    tom: fix path

--- setup-redis.sh
+++ setup-redis.sh
@@ -5,9 +5,9 @@ pkgin in redis
 # add slave line
 if [ "$REDIS_MASTER" ]; then
     echo "This Redis will be a slave of master: $REDIS_MASTER" 
-    echo "slaveof $REDIS_MASTER 6379" >> /opt/local/redis.conf
+    echo "slaveof $REDIS_MASTER 6379" >> /opt/local/etc/redis.conf
 else 
     echo "This Redis will be a master"
 fi
-
+sleep 5
 svcadm enable redis
